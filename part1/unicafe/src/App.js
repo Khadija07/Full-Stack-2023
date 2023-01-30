c3687105110570a4b5f0850c06901b0c211da3cc
Khadija07 <ktk_01@outlook.com>
2023-01-31 03:08:04 +0600

    unicafe step 6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,30 +1,100 @@
 import { useState } from 'react'
+const Statistics = (props) => {
+  return(
+    <div>
+    <p>good {props.good}</p>
+    <p>neutral {props.good}</p>
+    <p>bad {props.bad}</p>
+    <p>all {props.total}</p>
+    <p>average {props.average}</p>
+    <p>positive {props.positive}%</p>
+
+  </div>
+  )
+  
+}
 
 const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-   
-  const [selected, setSelected] = useState(0)
-  const handleClick = () => {
-    const updated = selected + 1
-    setSelected(updated)
-  } 
+  // save clicks of each button to its own state
 
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0) 
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState('0')
+  const [positive, setPositive] = useState('0')
+
+  const handleGoodClick = () => {
+    const updatedGood = good + 1
+    const avgG = updatedGood*1
+    const avgB = bad*(-1)
+    const avg = avgG + avgB
+    setGood(updatedGood)
+    setTotal(updatedGood+neutral+bad)
+    setAverage(avg)
+    setPositive(updatedGood*100)
+    
+  }
+  const handleNeutralClick = () => {
+    const updatedNeutral = neutral + 1
+    const avgG = good * 1
+    const avgB = bad * (-1)
+    const avg = avgG + avgB
+    setNeutral(updatedNeutral)
+    setTotal(updatedNeutral+good+bad)
+    setAverage(avg)
+
+  }
+  const handleBadClick = () => {
+    const updatedBad = bad + 1
+    const avgB = updatedBad*(-1)
+    const avgG = good*1
+    const avg = avgG + avgB
+    setBad(updatedBad)
+    setTotal(updatedBad+good+neutral)
+    setAverage(avg)
+  } 
 
   return (
     <div>
-      {anecdotes[selected]}
-      <p><button onClick={handleClick}>next anecdote</button></p>
+      <h1>give feedback</h1>
+
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>good</td>
+            <td>{good}</td>
+          </tr>
+          <tr>
+            <td>neutral</td>
+            <td>{neutral}</td>
+          </tr>
+          <tr>
+            <td>bad</td>
+            <td>{bad}</td>
+          </tr>
+          <tr>
+            <td>all</td>
+            <td>{total}</td>
+          </tr>
+          <tr>
+            <td>average</td>
+            <td>{average/total}</td>
+          </tr>
+          <tr>
+            <td>positive</td>
+            <td>{positive/total} %</td>
+          </tr>
+        </tbody>
+      </table>
+
+      
     </div>
   )
 }
-
+                                                                                           
 export default App
